d7c0f83a3f307c662d2ac0312a1d0055fd710fee
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-03-07 21:51:45 +0100

    windows_examples: make helloworld_noimport prints a message in the console

--- windows_examples/helloworld_noimport.c
+++ windows_examples/helloworld_noimport.c
@@ -25,13 +25,16 @@ void __cdecl _start(void)
  */
 static int _main(void)
 {
-    int (WINAPI *pfnMessageBoxW)(
-        IN HWND hWnd OPTIONAL,
-        IN LPCWSTR lpText OPTIONAL,
-        IN LPCWSTR lpCaption OPTIONAL,
-        IN UINT uType
-    );
-    HMODULE hModule;
+    int (WINAPI *pfnMessageBoxW)(HWND hWnd, PCWSTR lpText, LPCWSTR lpCaption, UINT uType);
+    BOOL (WINAPI *pfnAllocConsole)(void);
+    BOOL (WINAPI *pfnFreeConsole)(void);
+    HANDLE (WINAPI *pfnGetStdHandle)(IN DWORD nStdHandle);
+    BOOL (WINAPI *pfnWriteFile)(
+        HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite,
+        LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped);
+    const char helloworld[] = "Hello, world!\n";
+
+    HMODULE hModule, hStdOut;
     hModule = _LoadLibraryW(L"user32.dll");
     if (!hModule) {
         return 1;
@@ -40,7 +43,23 @@ static int _main(void)
     if (!pfnMessageBoxW) {
         return 1;
     }
+
+    pfnAllocConsole = _GetKernel32ProcAddress("AllocConsole");
+    pfnFreeConsole = _GetKernel32ProcAddress("FreeConsole");
+    pfnGetStdHandle = _GetKernel32ProcAddress("GetStdHandle");
+    pfnWriteFile = _GetKernel32ProcAddress("WriteFile");
+
+    /* Open a console, even when using the windows subsystem */
+    if (pfnAllocConsole && pfnGetStdHandle && pfnWriteFile) {
+        pfnAllocConsole();
+        hStdOut = pfnGetStdHandle(STD_OUTPUT_HANDLE);
+        pfnWriteFile(hStdOut, helloworld, sizeof(helloworld) - 1, NULL, NULL);
+    }
+
     pfnMessageBoxW(NULL, L"Hello, world!", L"Hello world box", MB_ICONINFORMATION | MB_OK);
     _FreeLibrary(hModule);
+    if (pfnFreeConsole) {
+        pfnFreeConsole();
+    }
     return 0;
 }
