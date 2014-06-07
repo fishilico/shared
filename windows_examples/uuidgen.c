e81f101801953d7bf1100a533c807d310b2266c8
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-06-07 13:40:14 +0200

    windows_examples: add uuidgen

---
+++ windows_examples/uuidgen.c
@@ -0,0 +1,31 @@
+/**
+ * Generate an Universal Unique Identifier using Windows API
+ *
+ * Link with rpcrt4.dll
+ */
+#include <windows.h>
+#include <stdio.h>
+
+int main()
+{
+    LPCSTR szDesc;
+    UUID uuid;
+    RPC_STATUS stat = UuidCreate(&uuid);
+    if (stat == S_OK) {
+        szDesc = "Global UUID";
+    } else if (stat == RPC_S_UUID_LOCAL_ONLY) {
+        szDesc = "Local UUID";
+    } else if (stat == RPC_S_UUID_NO_ADDRESS) {
+        szDesc = "Local UUID (hardware issue)";
+    } else {
+        fprintf(stderr, "UuidCreate: error %08lx\n", stat);
+        return 1;
+    }
+
+    printf("%s: {%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}\n",
+        szDesc,
+        uuid.Data1, uuid.Data2, uuid.Data3,
+        uuid.Data4[0], uuid.Data4[1], uuid.Data4[2], uuid.Data4[3],
+        uuid.Data4[4], uuid.Data4[5], uuid.Data4[6], uuid.Data4[7]);
+    return 0;
+}
