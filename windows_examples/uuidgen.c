e23e4babbc88501119b003cff53c8ba71d94302f
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-06-07 18:22:12 +0200

    windows_examples/uuidgen: support unicode interface

--- windows_examples/uuidgen.c
+++ windows_examples/uuidgen.c
@@ -9,21 +9,21 @@
 
 int _tmain()
 {
-    LPCSTR szDesc;
+    LPCTSTR szDesc;
     UUID uuid;
     RPC_STATUS stat = UuidCreate(&uuid);
     if (stat == S_OK) {
-        szDesc = "Global UUID";
+        szDesc = _T("Global UUID");
     } else if (stat == RPC_S_UUID_LOCAL_ONLY) {
-        szDesc = "Local UUID";
+        szDesc = _T("Local UUID");
     } else if (stat == RPC_S_UUID_NO_ADDRESS) {
-        szDesc = "Local UUID (hardware issue)";
+        szDesc = _T("Local UUID (hardware issue)");
     } else {
-        fprintf(stderr, "UuidCreate: error %08lx\n", stat);
+        _ftprintf(stderr, _T("UuidCreate: error 0x%08lx\n"), stat);
         return 1;
     }
 
-    printf("%s: {%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}\n",
+    _tprintf(_T("%s: {%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}\n"),
         szDesc,
         uuid.Data1, uuid.Data2, uuid.Data3,
         uuid.Data4[0], uuid.Data4[1], uuid.Data4[2], uuid.Data4[3],
