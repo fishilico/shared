7e8f6b662a05db6f0e89e3cbd68ea84bc1c75580
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-07-07 21:18:34 +0200

    linux_examples/modules: add a basic linux kernel module

---
+++ linux_examples/modules/hello_world.c
@@ -0,0 +1,34 @@
+/**
+ * Log messages in the kernel ring buffer
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/version.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nicolas Iooss");
+MODULE_DESCRIPTION("Simple kernel module which says hello and bye");
+
+static int num = 42;
+module_param(num, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+MODULE_PARM_DESC(num, "A number");
+
+static int __init hello_world_init(void)
+{
+	pr_info("Hello, world! The \"num\" param is %d\n", num);
+	pr_info("You can change it by writing to /sys/module/"KBUILD_MODNAME"/parameters/num\n");
+	pr_info("LINUX_VERSION_CODE is 0x%06x\n", LINUX_VERSION_CODE);
+	return 0;
+}
+
+static void __exit hello_world_exit(void)
+{
+	pr_info("Goodbye, world! The \"num\" param was %d\n", num);
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
