20f319aae3f4f39f960028c263423c5c99e3b18d
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-09-05 17:54:19 +0200

    Makefiles: add os-detect.mk

---
+++ os-detect.mk
@@ -0,0 +1,24 @@
+# Detect which OS the host is and include the corresponding flags.mk file
+
+# Include files from this directory
+TOPDIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+ifeq ($(OS), Windows_NT)
+include $(TOPDIR)windows-flags.mk
+BIN_EXT := .exe
+CC = $(WINCC)
+
+# Don't use wide characters
+CPPFLAGS := $(filter-out -D_UNICODE, $(CPPFLAGS))
+CFLAGS := $(filter-out -municode, $(CFLAGS))
+LDFLAGS := $(filter-out -municode, $(LDFLAGS))
+
+else # !Windows
+
+BIN_EXT := .bin
+UNAME_s := $(shell $(UNAME) -s)
+ifeq ($(UNAME_s), Linux)
+include $(TOPDIR)linux-flags.mk
+endif # Linux
+
+endif # Windows
