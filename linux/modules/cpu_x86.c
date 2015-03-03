fa57d9b7977a81076e4451f75fabfa5a4ce9f9ce
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2015-03-03 21:15:26 +0800

    linux/modules/cpu_x86: add output example for x86_64
    This avoids the need to compile and load the module each time I look for
    information about x86 registers, tables and constants.

--- linux/modules/cpu_x86.c
+++ linux/modules/cpu_x86.c
@@ -26,6 +26,46 @@
 #define show_cr_bit(cr, crnum, bitname, desc) \
 	show_reg_bit((cr), #bitname, X86_CR##crnum##_##bitname##_BIT, (desc))
 
+/**
+ * Output example on x86_64:
+ *      cr0 = 0x8005003b
+ *         0 (0x00000001): + PE (Protection Enable)
+ *         1 (0x00000002): + MP (Monitor Coprocessor)
+ *         2 (0x00000004): - EM (Emulation)
+ *         3 (0x00000008): + TS (Task Switched)
+ *         4 (0x00000010): + ET (Extension Type)
+ *         5 (0x00000020): + NE (Numeric Error)
+ *        16 (0x00010000): + WP (Write Protect)
+ *        18 (0x00040000): + AM (Alignment Mask)
+ *        29 (0x20000000): - NW (Not Write-through)
+ *        30 (0x40000000): - CD (Cache Disable)
+ *        31 (0x80000000): + PG (Paging)
+ *      cr2 = 0x0229e678 PFLA (Page Fault Linear Address)
+ *      cr3 = 0x2b8c08000
+ *         3 (0x00000008): - PWT (Page Write Through)
+ *         4 (0x00000010): - PCD (Page Cache Disable)
+ *        12...: 0x002b8c08 PDBR (Page Directory Base Register)
+ *      cr4 = 0x000006f0
+ *         0 (0x00000001): - VME (enable vm86 extensions)
+ *         1 (0x00000002): - PVI (virtual interrupts flag enable)
+ *         2 (0x00000004): - TSD (disable time stamp at ipl 3)
+ *         3 (0x00000008): - DE (enable debugging extensions)
+ *         4 (0x00000010): + PSE (enable page size extensions)
+ *         5 (0x00000020): + PAE (enable physical address extensions)
+ *         6 (0x00000040): + MCE (Machine check enable)
+ *         7 (0x00000080): + PGE (enable global pages)
+ *         8 (0x00000100): - PCE (enable performance counters at ipl 3)
+ *         9 (0x00000200): + OSFXSR (enable fast FPU save and restore)
+ *        10 (0x00000400): + OSXMMEXCPT (enable unmasked SSE exceptions)
+ *        13 (0x00002000): - VMXE (enable VMX virtualization)
+ *        14 (0x00004000): - SMXE (enable safer mode (TXT))
+ *        16 (0x00010000): - FSGSBASE (enable RDWRFSGS support)
+ *        17 (0x00020000): - PCIDE (enable PCID support)
+ *        18 (0x00040000): - OSXSAVE (enable xsave and xrestore)
+ *        20 (0x00100000): - SMEP (enable SMEP support)
+ *        21 (0x00200000): - SMAP (enable SMAP support)
+ *      cr8 = 0x0 TPR (Task-Priority Register)
+ */
 static void dump_x86_cr(void)
 {
 	unsigned long cr;
@@ -82,19 +122,41 @@ static void dump_x86_cr(void)
 #define show_efer_bit(efer, bitname, desc) \
 	show_reg_bit((efer), #bitname, _EFER_##bitname, (desc))
 
+/**
+ * Output example on x86_64:
+ *      msr:fs_base(0xc0000100) = 0x7f6ae5613700
+ *      msr:gs_base(0xc0000101) = 0xffff88003fc00000
+ *      msr:kernel_gs_base(0xc0000102) = 0x0
+ *      msr:EFER(0xc0000080) = 0x00000d01 (Extended Feature Enable Register)
+ *         0 (0x00000001): + SCE (SYSCALL/SYSRET)
+ *         8 (0x00000100): + LME (Long Mode Enable)
+ *        10 (0x00000400): + LMA (Long Mode Active (read-only))
+ *        11 (0x00000800): + NX (No eXecute)
+ *        12 (0x00001000): - SVME (Virtualization Enable)
+ *        13 (0x00002000): - LMSLE (Long Mode Segment Limit Enable)
+ *        14 (0x00004000): - FFXSR (Fast FXSAVE/FXRSTOR)
+ *      msr:ia32_sysenter_CS(0x174) = 0x10
+ *      msr:ia32_sysenter_ESP(0x175) = 0x0
+ *      msr:ia32_sysenter_EIP(0x176) = 0xffffffff81732800 (ia32_sysenter_target+0x0/0x62)
+ *      msr:star(0xc0000081) = user32 CS 0x23, kernel CS 0x10, EIP 0x0
+ *      msr:lstar(0xc0000082) = 0xffffffff81730130 (system_call+0x0/0x3)
+ *      msr:cstar(0xc0000083) = 0xffffffff81732a40 (ia32_cstar_target+0x0/0x8c)
+ *      msr:sfmask(0xc0000084) = 0x47700
+ */
 static void dump_x86_msr(void)
 {
 #ifdef CONFIG_X86_64
 	unsigned long long msr;
 	rdmsrl(MSR_FS_BASE, msr);
-	pr_info("msr:fs_base = 0x%llx\n", msr);
+	pr_info("msr:fs_base(0x%x) = 0x%llx\n", MSR_FS_BASE, msr);
 	rdmsrl(MSR_GS_BASE, msr);
-	pr_info("msr:gs_base = 0x%llx\n", msr);
+	pr_info("msr:gs_base(0x%x) = 0x%llx\n", MSR_GS_BASE, msr);
 	rdmsrl(MSR_KERNEL_GS_BASE, msr);
-	pr_info("msr:kernel_gs_base = 0x%llx\n", msr);
+	pr_info("msr:kernel_gs_base(0x%x) = 0x%llx\n", MSR_KERNEL_GS_BASE, msr);
 
 	rdmsrl(MSR_EFER, msr);
-	pr_info("msr:EFER = 0x%08llx (Extended Feature Enable Register)\n", msr);
+	pr_info("msr:EFER(0x%x) = 0x%08llx (Extended Feature Enable Register)\n",
+		MSR_EFER, msr);
 	show_efer_bit(msr, SCE, "SYSCALL/SYSRET");
 	show_efer_bit(msr, LME, "Long Mode Enable");
 	show_efer_bit(msr, LMA, "Long Mode Active (read-only)");
@@ -104,27 +166,39 @@ static void dump_x86_msr(void)
 	show_efer_bit(msr, FFXSR, "Fast FXSAVE/FXRSTOR");
 
 	rdmsrl(MSR_IA32_SYSENTER_CS, msr);
-	pr_info("msr:ia32_sysenter_CS = 0x%llx\n", msr);
+	pr_info("msr:ia32_sysenter_CS(0x%x) = 0x%llx\n", MSR_IA32_SYSENTER_CS, msr);
 	rdmsrl(MSR_IA32_SYSENTER_ESP, msr);
-	pr_info("msr:ia32_sysenter_ESP = 0x%llx\n", msr);
+	pr_info("msr:ia32_sysenter_ESP(0x%x) = 0x%llx\n", MSR_IA32_SYSENTER_ESP, msr);
 	rdmsrl(MSR_IA32_SYSENTER_EIP, msr);
-	pr_info("msr:ia32_sysenter_EIP = 0x%llx (%pS)\n", msr, (void*)msr);
+	pr_info("msr:ia32_sysenter_EIP(0x%x) = 0x%llx (%pS)\n",
+		MSR_IA32_SYSENTER_EIP, msr, (void*)msr);
 
 	/* http://wiki.osdev.org/SYSENTER
 	 * Setup in /usr/src/linux/arch/x86/kernel/cpu/common.c
+	 * STAR = Syscall Target
 	 */
 	rdmsrl(MSR_STAR, msr);
-	pr_info("msr:star = user32 CS 0x%llx, kernel CS 0x%llx, EIP 0x%llx\n",
-		msr >> 48, (msr >> 32) & 0xffff, msr & 0xffffffff);
+	pr_info("msr:star(0x%x) = user32 CS 0x%llx, kernel CS 0x%llx, EIP 0x%llx\n",
+		MSR_STAR, msr >> 48, (msr >> 32) & 0xffff, msr & 0xffffffff);
 	rdmsrl(MSR_LSTAR, msr); /* Long mode */
-	pr_info("msr:lstar = 0x%llx (%pS)\n", msr, (void*)msr);
+	pr_info("msr:lstar(0x%x) = 0x%llx (%pS)\n", MSR_LSTAR, msr, (void*)msr);
 	rdmsrl(MSR_CSTAR, msr); /* Compatibility mode */
-	pr_info("msr:cstar = 0x%llx (%pS)\n", msr, (void*)msr);
+	pr_info("msr:cstar(0x%x) = 0x%llx (%pS)\n", MSR_CSTAR, msr, (void*)msr);
 	rdmsrl(MSR_SYSCALL_MASK, msr);
-	pr_info("msr:sfmask = 0x%llx\n", msr);
+	pr_info("msr:sfmask(0x%x) = 0x%llx\n", MSR_SYSCALL_MASK, msr);
 #endif
 }
 
+/**
+ * Output example on x86_64:
+ *      CS = 0x0010 (kernel CS is 0x0010)
+ *      DS = 0x0000 (current thread: 0x0000)
+ *      ES = 0x0000 (current thread: 0x0000)
+ *      FS = 0x0000 (current thread: 0x0000)
+ *      GS = 0x0000 (current thread: 0x0000)
+ *      SS = 0x0018 (kernel DS is 0x0018)
+ *      TR: 0x00000040 (Task Register)
+ */
 static void dump_x86_segments(void)
 {
 	unsigned long seg;
@@ -142,6 +216,112 @@ static void dump_x86_segments(void)
 	pr_info("SS = 0x%04lx (kernel DS is 0x%04x)\n", seg, __KERNEL_DS);
 }
 
+/**
+ * Output example on x86_64:
+ *      GDT: (Global Descriptor Table)
+ *        On current cpu: 0xffff88003fc0a000 limit 127
+ *        CPU 0: 0xffff88003fc0a000 (16 entries)
+ *           1: base 0x00000000, limit 0xfffff, flags 0xc09b (Kernel32 CS)
+ *              type=0xb (C-RA), s=1, dpl=0, p=1, avl=0, l=0, d=1, g=1
+ *           2: base 0x00000000, limit 0xfffff, flags 0xa09b (Kernel CS)
+ *              type=0xb (C-RA), s=1, dpl=0, p=1, avl=0, l=1, d=0, g=1
+ *           3: base 0x00000000, limit 0xfffff, flags 0xc093 (Kernel DS)
+ *              type=0x3 (D-WA), s=1, dpl=0, p=1, avl=0, l=0, d=1, g=1
+ *           4: base 0x00000000, limit 0xfffff, flags 0xc0fb (Default user32 CS)
+ *              type=0xb (C-RA), s=1, dpl=3, p=1, avl=0, l=0, d=1, g=1
+ *           5: base 0x00000000, limit 0xfffff, flags 0xc0f3 (Default user DS)
+ *              type=0x3 (D-WA), s=1, dpl=3, p=1, avl=0, l=0, d=1, g=1
+ *           6: base 0x00000000, limit 0xfffff, flags 0xa0fb (Default user CS)
+ *              type=0xb (C-RA), s=1, dpl=3, p=1, avl=0, l=1, d=0, g=1
+ *           8: base 0x3fc11c80, limit 0x2087, flags 0x008b (TSS, Task State Segment)
+ *              type=0xb (C-RA), s=0, dpl=0, p=1, avl=0, l=0, d=0, g=0
+ *           9: base 0x0000ffff, limit 0x8800, flags 0x0000 (TSS+1)
+ *              type=0x0 (D---), s=0, dpl=0, p=0, avl=0, l=0, d=0, g=0
+ *          15: base 0x00000000, limit 0x0, flags 0x00f4 (per CPU)
+ *              type=0x4 (Dv--), s=1, dpl=3, p=1, avl=0, l=0, d=0, g=0
+ *      IDT: 0xffffffffff56b000 limit 4095 (Interrupt Descriptor Table, 256 entries)
+ *       0x00: seg 0x10 offset ffffffff81731ab0 Divide by Zero
+ *             Sym: divide_error
+ *       0x01: seg 0x10 offset ffffffff81731f80 Debug
+ *             ist=4 (debug stack)
+ *             Sym: debug
+ *       0x02: seg 0x10 offset ffffffff817323e0 Non-maskable Interrupt
+ *             ist=3 (non-maskable interrupt stack)
+ *             Sym: nmi
+ *       0x03: seg 0x10 offset ffffffff81731fc0 Break Point
+ *             ist=4 (debug stack)
+ *             dpl=3
+ *             Sym: int3
+ *       0x04: seg 0x10 offset ffffffff81731ae0 Overflow
+ *             dpl=3
+ *             Sym: overflow
+ *       0x05: seg 0x10 offset ffffffff81731b10 Bound Range Exceeded
+ *             Sym: bounds
+ *       0x06: seg 0x10 offset ffffffff81731b40 Invalid Opcode
+ *             Sym: invalid_op
+ *       0x07: seg 0x10 offset ffffffff81731b70 Device Not Available
+ *             Sym: device_not_available
+ *       0x08: seg 0x10 offset ffffffff81731ba0 Double Fault
+ *             ist=2 (double fault stack)
+ *             Sym: double_fault
+ *       0x09: seg 0x10 offset ffffffff81731bd0 Coprocessor Segment Overrun
+ *             Sym: coprocessor_segment_overrun
+ *       0x0a: seg 0x10 offset ffffffff81731c00 Invalid TSS
+ *             Sym: invalid_TSS
+ *       0x0b: seg 0x10 offset ffffffff81731c30 Segment Not Present
+ *             Sym: segment_not_present
+ *       0x0c: seg 0x10 offset ffffffff81732000 Stack Segment Fault
+ *             ist=1 (stack fault stack)
+ *             Sym: stack_segment
+ *       0x0d: seg 0x10 offset ffffffff817320c0 General Protection Fault
+ *             Sym: general_protection
+ *       0x0e: seg 0x10 offset ffffffff81732150 Page Fault
+ *             Sym: async_page_fault
+ *       0x0f: seg 0x10 offset ffffffff81731c60 Spurious Interrupt
+ *             Sym: spurious_interrupt_bug
+ *       0x10: seg 0x10 offset ffffffff81731c90 x87 Floating-Point Exception
+ *             Sym: coprocessor_error
+ *       0x11: seg 0x10 offset ffffffff81731cc0 Alignment Check
+ *             Sym: alignment_check
+ *       0x12: seg 0x10 offset ffffffff81732180 Machine Check
+ *             ist=5 (machine check stack)
+ *             Sym: machine_check
+ *       0x13: seg 0x10 offset ffffffff81731cf0 SIMD Floating-Point Exception
+ *             Sym: simd_coprocessor_error
+ *       0x20: seg 0x10 offset ffffffff81730ec0 IRET Exception
+ *             Sym: irq_move_cleanup_interrupt
+ *       0x80: seg 0x10 offset ffffffff81732c90 Syscall Vector
+ *             dpl=3
+ *             Sym: ia32_syscall
+ *       0xef: seg 0x10 offset ffffffff81731030 Local Timer Vector
+ *             Sym: apic_timer_interrupt
+ *       0xf2: seg 0x10 offset ffffffff81731230 Postr Intr Vector
+ *             Sym: kvm_posted_intr_ipi
+ *       0xf3: seg 0x10 offset ffffffff81730c04 Hypervisor Callback Vector
+ *             Sym: irq_entries_start+0x3c4/0x400
+ *       0xf5: seg 0x10 offset ffffffff81730c0c UV Bau Message
+ *             Sym: irq_entries_start+0x3cc/0x400
+ *       0xf6: seg 0x10 offset ffffffff817319b0 IRQ Work Vector
+ *             Sym: irq_work_interrupt
+ *       0xf7: seg 0x10 offset ffffffff81731130 x86 Plateform IPI Vector
+ *             Sym: x86_platform_ipi
+ *       0xf8: seg 0x10 offset ffffffff81730f30 Reboot Vector
+ *             Sym: reboot_interrupt
+ *       0xf9: seg 0x10 offset ffffffff817312b0 Threshold APIC Vector
+ *             Sym: threshold_interrupt
+ *       0xfa: seg 0x10 offset ffffffff817313b0 Thermal APIC Vector
+ *             Sym: thermal_interrupt
+ *       0xfb: seg 0x10 offset ffffffff817314b0 Call Function Single Vector
+ *             Sym: call_function_single_interrupt
+ *       0xfc: seg 0x10 offset ffffffff817315b0 Call Function Vector
+ *             Sym: call_function_interrupt
+ *       0xfd: seg 0x10 offset ffffffff817316b0 Reschedule Vector
+ *             Sym: reschedule_interrupt
+ *       0xfe: seg 0x10 offset ffffffff817317b0 Error APIC Vector
+ *             Sym: error_interrupt
+ *       0xff: seg 0x10 offset ffffffff817318b0 Spurious APIC Vector
+ *             Sym: spurious_interrupt
+ */
 static void dump_x86_tables(void)
 {
 	unsigned int cpu, i, numentries;
