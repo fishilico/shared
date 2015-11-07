b77d29834b90549116b30bf9d8c210ec85e1c2a8
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2015-11-07 19:42:56 +0100

    machines: add Dockerfiles
    Use Docker to test the project on several releases of:
    * Arch Linux
    * Debian
    * Fedora
    * Ubuntu
    
    For each distribution, install the necessary packages and run tests for
    several configurations

---
+++ machines/docker_build_run.sh
@@ -0,0 +1,208 @@
+#!/bin/sh
+# Build and run the specified docker image
+
+# Ensure we are running inside the directory containing Dockerfiles
+cd "$(dirname -- "$0")" || exit $?
+
+DO_BUILD=true
+DO_RUN=true
+DO_PRETEND=false
+USE_VOLUME=false
+IMAGE_NAME=''
+
+# Get a base name for an image from a name which comes from the command line
+get_base_name() {
+    local NAME
+    NAME="$1"
+
+    # Drop "Dockerfile-" prefix, which allows using file name completion
+    NAME="${NAME#Dockerfile-}"
+    # Drop special symbols
+    NAME="$(echo "$NAME" | tr -cd 'a-zA-Z0-9' | tr '[:upper:]' '[:lower:]')"
+    # Recognize keywords
+    case "$NAME" in
+        *arch*)
+            echo "archlinux"
+            ;;
+
+        *debian6*|*squeeze*)
+            echo "debian6-squeeze"
+            ;;
+        *debian7*|*wheezy*)
+            echo "debian7-wheezy"
+            ;;
+        *debian8*|*jessie*)
+            echo "debian8-jessie"
+            ;;
+        *debian9*|*stretch*)
+            echo "debian9-stretch"
+            ;;
+
+        *ubuntu1204*|*precise*)
+            echo "ubuntu1204-precise"
+            ;;
+        *ubuntu1404*|*trusty*)
+            echo "ubuntu1404-trusty"
+            ;;
+        *ubuntu1504*|*vivid*)
+            echo "ubuntu1504-vivid"
+            ;;
+        *ubuntu1510*|*wily*)
+            echo "ubuntu1510-wily"
+            ;;
+
+        *fedora20*|*heidenbug*)
+            echo "fedora20-heisenbug"
+            ;;
+        *fedora21*|*rawhide*)
+            echo "fedora21-rawhide"
+            ;;
+        *fedora22*)
+            echo "fedora22"
+            ;;
+    esac
+}
+
+get_available_bases() {
+    # Generated with:
+    # sed -n '/^get_base_name/,/^get_available_bases/{s/^ *\(echo \)/\1/p}' docker_build_run.sh
+    echo "archlinux"
+    echo "debian6-squeeze"
+    echo "debian7-wheezy"
+    echo "debian8-jessie"
+    echo "debian9-stretch"
+    echo "ubuntu1204-precise"
+    echo "ubuntu1404-trusty"
+    echo "ubuntu1504-vivid"
+    echo "ubuntu1510-wily"
+    echo "fedora20-heisenbug"
+    echo "fedora21-rawhide"
+    echo "fedora22"
+}
+
+while getopts "bhn:prv" OPT
+do
+    case "$OPT" in
+        h)
+            echo "Usage: $0 [OPTION] NAME"
+            echo "Build and run a docker image from the system NAME"
+            echo "If NAME is 'all', build every for possible system"
+            echo ""
+            echo "Options:"
+            echo "  -b       only build an image"
+            echo "  -h       display this help and exit"
+            echo "  -n IMG   define the tag name of the built image"
+            echo "  -p       pretend mode, do not do anyting"
+            echo "  -r       only run a pre-built image"
+            echo "  -v       use a volume when running, to link to git code"
+            exit
+            ;;
+        b)
+            DO_BUILD=true
+            DO_RUN=false
+            ;;
+        n)
+            IMAGE_NAME="$OPTARG"
+            ;;
+        p)
+            DO_PRETEND=true
+            ;;
+        r)
+            DO_BUILD=false
+            DO_RUN=true
+            ;;
+        v)
+            USE_VOLUME=true
+            ;;
+        \?)
+            echo >&2 "Try '$0 -h' for more information."
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+if [ $# -lt 1 ]
+then
+    echo >&2 "$0: missing name argument"
+    echo >&2 "Use '$0 -h' for more information."
+    exit 1
+fi
+
+build_and_run() {
+    # Build image
+    if "$DO_BUILD"
+    then
+        if "$DO_PRETEND"
+        then
+            echo "docker build -f 'Dockerfile-$BASE_NAME' -t '$IMAGE_NAME' .."
+        else
+            docker build -f "Dockerfile-$BASE_NAME" -t "$IMAGE_NAME" .. || return $?
+        fi
+    fi
+
+    # Run it, with parameters from the command line
+    if "$DO_RUN"
+    then
+        if "$USE_VOLUME"
+        then
+            if "$DO_PRETEND"
+            then
+                echo "docker run -t -v '$(pwd)/..:/shared' -i '$IMAGE_NAME' $*"
+            else
+                # Run docker with a pseudo-TTY and with a volume
+                docker run -t -v "$(pwd)/..:/shared" -i "$IMAGE_NAME" "$@" || return $?
+            fi
+        else
+            # Run docker without a volume to base directory
+            if "$DO_PRETEND"
+            then
+                echo "docker run -t -i '$IMAGE_NAME' $*"
+            else
+                docker run -t -i "$IMAGE_NAME" "$@" || return $?
+            fi
+        fi
+    fi
+}
+
+if [ "$1" = 'all' ]
+then
+    shift
+    for BASE_NAME in $(get_available_bases)
+    do
+        IMAGE_NAME="shared-$BASE_NAME"
+        echo "----------------------------------------------------------------"
+        echo "            Using image $IMAGE_NAME..."
+        echo "----------------------------------------------------------------"
+        if ! build_and_run "$@"
+        then
+            echo >&2 "Failed with base $BASE_NAME"
+            exit 1
+        fi
+    done
+elif [ "$1" = 'purge' ]
+then
+    # Purge the Docker images
+    for BASE_NAME in $(get_available_bases)
+    do
+        IMAGE_NAME="shared-$BASE_NAME"
+        if [ -n "$(docker images -q "$IMAGE_NAME")" ]
+        then
+            docker rmi "shared-$BASE_NAME" || exit $?
+        fi
+    done
+else
+    BASE_NAME="$(get_base_name "$1")"
+    if [ -z "$BASE_NAME" ]
+    then
+        echo >&2 "Unknown name '$1'"
+        exit 1
+    fi
+    shift
+
+    if [ -z "$IMAGE_NAME" ]
+    then
+        IMAGE_NAME="shared-$BASE_NAME"
+    fi
+
+    build_and_run "$@" || exit $?
+fi
---
+++ machines/run_shared_test.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Run the tests inside a machine
+
+if [ ! -d '/shared' ]
+then
+    echo >&2 "Error: /shared directory not found"
+    exit 1
+fi
+
+cd '/shared' || exit $?
+
+# Find installed kernel headers revision
+# * Debian package: linux-headers-amd64
+# * Ubuntu package: linux-headers-generic
+# * Fedora package: kernel-devel
+KERNELVER="$(LANG=C dpkg --status linux-headers-amd64 linux-headers-generic 2>/dev/null |
+    sed -n 's/^Depends: linux-headers-\(.*\)/\1/p' | head -n 1)"
+if [ -z "$KERNELVER" ]
+then
+    KERNELVER="$(LANG=C rpm -qi kernel-devel 2>/dev/null |
+        sed -n 's/^Source RPM *: kernel-\(.*\).src.rpm$/\1.x86_64/p' | tail -n 1)"
+fi
+export KERNELVER
+
+# Do not use unicode in MinGW<4.8, when "wmain" is not automatically declared
+case "$(x86_64-w64-mingw32-gcc --version | sed -n 's/^x86_64-w64-mingw32-gcc (GCC) //p')" in
+    [123].*|4.[0-7].*)
+        export HAVE_UNICODE=n
+        ;;
+esac
+
+echo '******************************************'
+echo '* Building with gcc                      *'
+echo '******************************************'
+make CC=gcc clean test || exit $?
+
+# Do not build the kernel modules with modified compiler
+KERNELVER=
+
+if [ -x /usr/bin/clang ] || clang --version 2>/dev/null
+then
+    echo '******************************************'
+    echo '* Building with clang                    *'
+    echo '******************************************'
+    make CC=clang clean test HAVE_OPENMP=n HAVE_GTK3=n HAVE_PYTHON_CFFI=n || exit $?
+fi
+
+if [ -x /usr/bin/musl-gcc ] || musl-gcc --version 2>/dev/null
+then
+    echo '******************************************'
+    echo '* Building with musl-gcc                 *'
+    echo '******************************************'
+    make CC=musl-gcc clean test HAVE_OPENMP=n HAVE_PULSE=n HAVE_SDL=n HAVE_PYTHON_CFFI=n || exit $?
+fi
+
+make list-nobuild
