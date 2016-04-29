f1939d6ffb7c170789a1ee9ca63fbdcbdbd480e5
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2016-04-29 15:12:27 +0200

    linux/special-pages/dump_kernel_pages: be resilient to SIGBUS
    Gentoo Hardened kernel does not seem to support reading the 2nd page of
    vvar zone.  strace reports:
    
        --- SIGBUS {si_signo=SIGBUS, si_code=BUS_ADRERR, si_addr=0x31480c40000} ---
        +++ killed by SIGBUS +++
        Bus error
    
    with:
    
        31480c3f000-31480c41000 r--p 00000000 00:00 0       [vvar]
    
    As this signal is raised in a call to ctypes.memmove, there is no easy
    way to handle this signal in pure Python.  Instead, use write() function
    to dump the pages and keep memmove() to kernel pages, as write() cannot
    read them.

--- linux/special-pages/dump_kernel_pages.py
+++ linux/special-pages/dump_kernel_pages.py
@@ -2,12 +2,56 @@
 # -*- coding:UTF-8 -*-
 """Extract all special pages mapped by Linux kernel into process memory"""
 import ctypes
+import ctypes.util
+import errno
 import sys
 
 
 if sys.version_info < (2, 7):
     sys.stderr.write("This program cannot be run in Python<2.7 mode.\n")
     sys.exit(0)
+
+
+libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+libc.write.argtypes = [ctypes.c_int, ctypes.c_void_p, ctypes.c_size_t]
+libc.write.restype = ctypes.c_ssize_t
+
+
+def is_kernel_addr(addr):
+    if sys.maxsize > 2**32:
+        # 64-bit kernels always have the high bit set
+        return addr >= 2**63
+    else:
+        # 32-bit kernels may start at 0xc0000000 but each user-exposed page
+        # lies above 0xffff0000
+        return addr >= 0xffff0000
+
+
+def raw_write(fileout, addr, size):
+    """Write a raw buffer into a file"""
+    while size > 0:
+        count = libc.write(fileout.fileno(), addr, size)
+        if count < 0:
+            if ctypes.get_errno() != errno.EFAULT:
+                raise OSError(ctypes.get_errno())
+            # There was a fault while reading the address.
+            # This may be caused by kernel addresses readable in userspace
+            if is_kernel_addr(addr):
+                data = (ctypes.c_byte * size)()
+                ctypes.memmove(data, ctypes.c_void_p(addr), size)
+                count = fout.write(data)
+                # In Python2 count is None and in Python3 count is size
+                assert count is None or count == size
+            else:
+                # This happens while reading vvar on Gentoo Hardened.
+                # Truncate the file.
+                print("... Unable to read {} bytes from {:x}".format(
+                    size, addr))
+            return
+
+        assert count <= size
+        addr += count
+        size -= count
 
 
 # Enumerate memory ranges with /proc/self/maps
@@ -25,11 +69,9 @@ with open('/proc/self/maps', 'r') as fmaps:
         # Retrieve the pages
         addr1, addr2 = [int(addr, 16) for addr in sline[0].split('-', 1)]
         assert addr1 < addr2
-        data = (ctypes.c_byte * (addr2 - addr1))()
-        ctypes.memmove(data, ctypes.c_void_p(addr1), addr2 - addr1)
 
-        # Save file
+        # Write the pages to a file
         filename = 'kernel-{}.out'.format(name)
         print("{}: {:x}..{:x} {}".format(filename, addr1, addr2, sline[1]))
         with open(filename, 'wb') as fout:
-            fout.write(data)
+            raw_write(fout, addr1, addr2 - addr1)
