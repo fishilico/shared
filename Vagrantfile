04b80be515829b01507809c47bf7fd9a272be135
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2018-09-16 13:35:18 +0200

    Vagrant: add configuration for a Vagrant virtual machine
    Testing Docker containers work more reliably in a VM. Set up one which
    runs all the tests.

---
+++ Vagrantfile
@@ -0,0 +1,70 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Run tests:
+#    echo 'make -C /vagrant clean test' | vagrant ssh
+# Run tests in Docker containers:
+#    echo '/vagrant/machines/docker_test_all.sh' | vagrant ssh
+# Run tests and record "make list-nobuild" output:
+#    script -c "echo '/vagrant/machines/docker_test_all.sh' | vagrant ssh" dockers.log
+#    ./machines/update_list_nobuild_from_log.py dockers.log
+
+# Provisioning script: update the system, install docker and create an Arch Linux Docker image
+$script = <<SCRIPT
+timedatectl set-timezone UTC
+sed -i -e 's/^#\\?MAKEFLAGS=.*/MAKEFLAGS="-j\\$(nproc)"/' /etc/makepkg.conf
+
+# Update archlinux-keyring before the other packages in order to make sure the
+# recently-added packagers are known.
+pacman --noconfirm -Sy
+if [ "$(pacman -Qi archlinux-keyring | grep '^Version')" != "$(pacman -Si archlinux-keyring | grep '^Version')" ]
+then
+    pacman --noconfirm -S archlinux-keyring
+fi
+pacman --noconfirm -Su
+
+# Make sure Python is installed
+pacman -Qqi python > /dev/null || pacman --noconfirm -S python
+
+# Install Docker and build Arch Linux Docker image
+pacman -Qqi docker > /dev/null || pacman --noconfirm -S docker
+systemctl enable --now docker
+gpasswd -a vagrant docker
+(docker images |grep -q '^archlinux ') || docker build -t archlinux /vagrant/machines/archlinux
+
+# Use vsyscall=emulate on the command line, for Debian 7 Wheezy
+if [ -e /boot/syslinux/syslinux.cfg ] && ! grep vsyscall=emulate /boot/syslinux/syslinux.cfg > /dev/null
+then
+    sed -i -e 's/^\\(\\s*APPEND\\s.*\\)/\\1 vsyscall=emulate/' /boot/syslinux/syslinux.cfg
+    syslinux-install_update -i -a -m
+fi
+
+# Make sure that the number of kernel file handles stays reasonable by defining
+# a limit in /proc/sys/fs/file-max
+echo 'fs.file-max = 10000' > /etc/sysctl.d/99-sane-open-files-limit.conf
+SCRIPT
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  # Use Arch Linux Vagrant Base box
+  # https://wiki.archlinux.org/index.php/Vagrant#Base_Boxes_for_Vagrant
+  # For example with https://github.com/elasticdog/packer-arch and libvirt:
+  #    cd packer-arch
+  #    ./wrapacker -p libvirt
+  #    vagrant box add archlinux output/packer_arch_libvirt.box
+  config.vm.box = "archlinux"
+
+  config.vm.provider :libvirt do |v|
+    v.cpus = 4
+    v.memory = 2048
+  end
+
+  # Run the provisioning script
+  config.vm.provision :shell, inline: $script
+
+  # Ensure the build system works fine, and show what would run
+  config.vm.provision :shell, inline: "make -C /vagrant list-nobuild"
+end
---
+++ machines/docker_test_all.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+# Build and run all Docker images in order to perform tests
+# If running on a system with low storage, the images are automatically removed
+
+# Ensure we are running inside the directory containing Dockerfiles
+cd "$(dirname -- "$0")" || exit $?
+
+purge_docker_cache() {
+    local UNTAGGED_NUM NONE_VER_NUM
+
+    # Remove untagged images
+    UNTAGGED_NUM="$(docker images | awk '{ if($1 == "<none>")print $3 }' | wc -l)"
+    echo "There are currently $UNTAGGED_NUM untagged images."
+    if [ "$UNTAGGED_NUM" -gt 0 ]
+    then
+        docker images | awk '{ if($1 == "<none>")print $3 }' | xargs docker rmi
+        UNTAGGED_NUM="$(docker images | awk '{ if($1 == "<none>")print $3 }' | wc -l)"
+        echo "After cleanup, there remains $UNTAGGED_NUM untagged images."
+    fi
+
+    # Remove untagged versions (which appear after an update)
+    NONE_VER_NUM="$(docker images | awk '{ if($2 == "<none>")print $3 }' | wc -l)"
+    echo "There are currently $NONE_VER_NUM images with untagged versions."
+    if [ "$NONE_VER_NUM" -gt 0 ]
+    then
+        docker images | awk '{ if($2 == "<none>")print $3 }' | xargs docker rmi
+        NONE_VER_NUM="$(docker images | awk '{ if($2 == "<none>")print $3 }' | wc -l)"
+        echo "After cleanup, there remains $NONE_VER_NUM images with untagged versions."
+    fi
+}
+
+test_dockerfile() {
+    local DOCKERFILE BASE_NAME IMAGE_NAME DISK_SIZE
+
+    DOCKERFILE="$1"
+    BASE_NAME="${DOCKERFILE#Dockerfile-}"
+    IMAGE_NAME="shared-$BASE_NAME"
+
+    # Grab the previous Docker image ID, if any
+    PREVIOUS_IMAGE_ID="$(docker images --format '{{.ID}}' "$IMAGE_NAME" 2>/dev/null)"
+
+    echo "----------------------------------------------------------------"
+    echo "            Using image $IMAGE_NAME..."
+    echo "----------------------------------------------------------------"
+    if ! ./docker_build_run.sh "$BASE_NAME"
+    then
+        echo >&2 "Failed with base $BASE_NAME"
+        exit 1
+    fi
+
+    # Remove the previous image if it is different
+    if [ -n "$PREVIOUS_IMAGE_ID" ]
+    then
+        NEW_IMAGE_ID="$(docker images --format '{{.ID}}' "$IMAGE_NAME" 2>/dev/null)"
+        if [ "$NEW_IMAGE_ID" != "$PREVIOUS_IMAGE_ID" ]
+        then
+            PREV_IMAGE_LINE="$(docker images --format '{{.ID}} {{.Repository}}:{{.Tag}}' |grep "$PREVIOUS_IMAGE_ID")"
+            if [ "$PREV_IMAGE_LINE" != "$PREVIOUS_IMAGE_ID <none>:<none>" ]
+            then
+                echo >&2 "Unexpected docker image line for previous image of $IMAGE_NAME ($PREVIOUS_IMAGE_ID):"
+                echo >&2 "    $PREV_IMAGE_LINE"
+                return 1
+            fi
+            echo "Removing previous image of $IMAGE_NAME ($PREVIOUS_IMAGE_ID)"
+            docker rmi "$PREVIOUS_IMAGE_ID"
+        fi
+    fi
+
+    # Purge the image if there is little room available
+    DISK_SIZE="$(LANG=C df --output=size --block-size=G /var/lib/docker | sed -n '$s/G$//p' | tr -d ' ')"
+    if [ "$DISK_SIZE" -lt 50 ]
+    then
+        echo "Removing image $IMAGE_NAME as the disk size is $DISK_SIZE GB"
+        docker rmi "$IMAGE_NAME" || exit $?
+        purge_docker_cache
+    else
+        echo "Keeping image $IMAGE_NAME as the disk size is $DISK_SIZE GB"
+    fi
+}
+
+# If the input is not a TTY, create one with script
+# Otherwise "docker run -t" complains with "The input device is not a TTY"
+if ! [ -t 0 ]
+then
+    echo "Re-executing using script in order to get a TTY"
+    exec script -qe -c "./docker_test_all.sh $*" /dev/null
+fi
+
+# Allow specifying Dockerfiles directly on the command line
+if [ "$#" -gt 0 ]
+then
+    while [ "$#" -gt 0 ]
+    do
+        test_dockerfile "$1" || exit $?
+        shift
+    done
+else
+    for DOCKERFILE in $(find Dockerfile-* | sort)
+    do
+        test_dockerfile "$DOCKERFILE" || exit $?
+    done
+fi
