3ac6741ab1314ebffb260d260b1414d924f17b60
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2019-06-12 22:10:56 +0200

    web/upload_file: increase verbosity
    Make the script easier to use in a context of uploading files to a
    server I own.

--- web/upload_file.php
+++ web/upload_file.php
@@ -1,6 +1,17 @@
 <?php
 /*
- * Upload a file in an "upload" directory.
+ * Upload a file in an directory, upload/
+ *
+ * NB. Do not use this file as-is. This is an arbitrary file upload with
+ * arbitrary extension, so it would introduce a Remote Code Execution (RCE)
+ * vulnerability, because it allows uploading a PHP file ending with .php,
+ * .php3, .phtml, etc.
+ * If you want to use this file, please evaluate the security of this.
+ * If you still want to use this file, you may consider adding a password with
+ * something like:
+ *
+ *     $pwhash = '$6$...';
+ *     hash_equals($pwhash, crypt(@$_GET['p'], $pwhash)) or die('Forbidden');
  */
 if (!empty($_FILES['file'])) {
     $tmp_file = $_FILES['file']['tmp_name'];
@@ -14,17 +25,29 @@ if (!empty($_FILES['file'])) {
 }
 ?>
 <!doctype html>
-<html>
+<html lang="en">
     <head>
         <title>Upload</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     </head>
     <body>
+<?php if (!empty($_FILES['file'])): ?>
+        <p>
+            <?php
+                echo 'Uploaded <code>' . htmlentities($tmp_file) . '</code>' .
+                    ' to <code>' . htmlentities($new_file) . '</code>' .
+                    ' (' . $size . ' bytes, ' . htmlentities($mime_type) . ')' . PHP_EOL;
+            ?>
+        </p>
+<?php endif ?>
+<?php if (!is_dir('upload')): ?>
+        <p>Warning: directory <code>upload/</code> does not exist!</p>
+<?php endif ?>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
             <input type="file" name="file">
             <input type="submit" value="Upload">
         </form>
-        <p>php.ini configuration:
+        <p>php.ini configuration:</p>
         <ul>
 <?php
     foreach(array(
@@ -35,7 +58,7 @@ if (!empty($_FILES['file'])) {
         'upload_tmp_dir') as $k)
     {
         $v = ini_get($k);
-        echo '<li>' . $k . ' = ' . htmlentities($v) . PHP_EOL;
+        echo '<li><code>' . $k . '</code> = ' . htmlentities($v) . PHP_EOL;
     }
 ?>
         </ul>
