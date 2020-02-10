aea3425703bd71b94e701c9c977d4c0f2af873a1
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2020-02-10 21:53:52 +0100

    windows/list_cmdlines: support 64-bit Windows 10
    Using a remote Thread to return the result of GetCommandLineW does not
    work well on 64-bit systems. Moreover on Windows 10, several UWP
    applications are suspended and threads are created suspended there,
    without a proper way to detect such a thing. Work around this issue by
    introducing a timeout of 1 seconds.
    
    Moreover, use NtQueryInformationProcess() to retrieve the address of the
    PEB of the target process and read the command line from
    PEB->ProcessParameters->CommandLine.Buffer. This is much more stable
    anyway.

--- windows/list_cmdlines.c
+++ windows/list_cmdlines.c
@@ -1,5 +1,12 @@
 /**
- * List the command lines of the currently running processes
+ * List the command lines of the currently running processes using a remote thread.
+ *
+ * This works because kernel32.dll is always mapped at the same address in
+ * every process.
+ * A more stable way of doing this is using WMI, for example in PowerShell:
+ *
+ *     Get-WmiObject -query 'SELECT ProcessId, Name, CommandLine from Win32_Process' |
+ *       Format-List -Property ('ProcessId','Name','CommandLine')
  *
  * API documentation:
  *  HANDLE WINAPI CreateRemoteThread(
@@ -13,12 +20,47 @@
  *  );
  */
 #include "common.h"
+#include <inttypes.h>
 #include <tlhelp32.h>
+
+typedef DWORD (WINAPI * pfnNtQueryInformationProcess_t) (
+    HANDLE ProcessHandle,
+    int ProcessInformationClass,
+    PVOID ProcessInformation,
+    ULONG ProcessInformationLength,
+    PULONG ReturnLength);
+
+/* PROCESS_BASIC_INFORMATION structure */
+typedef struct _PROCESS_BASIC_INFORMATION_FOR_PEB {
+    PVOID Reserved1;
+    PVOID PebBaseAddress;
+    PVOID Reserved2[2];
+    ULONG_PTR UniqueProcessId;
+    PVOID Reserved3;
+} PROCESS_BASIC_INFORMATION_FOR_PEB;
+
+typedef struct _UNICODE_STRING_FOR_CMDLINE {
+    USHORT Length; /* Length in bytes */
+    USHORT MaximumLength;
+    PWSTR Buffer;
+} UNICODE_STRING_FOR_CMDLINE;
+
+/* Define offsets according to the architecture */
+#if defined(__x86_64)
+#    define PEB_offset_ProcessParameters 0x20
+#    define RTL_USER_PROCESS_PARAMETERS_offset_CommandLine 0x70
+#elif defined(__i386__)
+#    define PEB_offset_ProcessParameters 0x10
+#    define RTL_USER_PROCESS_PARAMETERS_offset_CommandLine 0x40
+#else
+#    warning Unsupported architecture
+#endif
 
 int _tmain(void)
 {
-    HMODULE hKernel;
+    HMODULE hKernel, hNtdll;
     LPTHREAD_START_ROUTINE lpfctGetCmdLine;
+    pfnNtQueryInformationProcess_t pfnNtQueryInformationProcess;
     HANDLE hProcessSnap;
     PROCESSENTRY32 pe32;
 
@@ -29,7 +71,20 @@ int _tmain(void)
     }
     lpfctGetCmdLine = (LPTHREAD_START_ROUTINE)(void *)GetProcAddress(hKernel, "GetCommandLineW");
     if (!lpfctGetCmdLine) {
-        print_winerr(_T("GetProcAddress(GetCommandLineW)"));
+        print_winerr(_T("GetProcAddress(kernel32, GetCommandLineW)"));
+        return 1;
+    }
+    _tprintf(_T("Using kernel32@%p->GetCommandLineW@%p\n"), hKernel, (void *)lpfctGetCmdLine);
+
+    hNtdll = GetModuleHandle(_T("ntdll.dll"));
+    if (!hKernel) {
+        print_winerr(_T("GetModuleHandle(ntdll.dll)"));
+        return 1;
+    }
+    pfnNtQueryInformationProcess =
+        (pfnNtQueryInformationProcess_t) (void *)GetProcAddress(hNtdll, "NtQueryInformationProcess");
+    if (!pfnNtQueryInformationProcess) {
+        print_winerr(_T("GetProcAddress(ntdll, NtQueryInformationProcess)"));
         return 1;
     }
 
@@ -48,19 +103,39 @@ int _tmain(void)
     do {
         HANDLE hModuleSnap, hProcess, hThread;
         MODULEENTRY32 me32;
-        DWORD dwRetAddr;
-        WCHAR szCmdLine[MAX_PATH * 8];
-        SIZE_T cbRead = 0;
+        DWORD dwRetAddr, dwStatus;
+        WCHAR wszCmdLine[MAX_PATH * 8];
+        SIZE_T cbRead;
+        ULONG length = 0;
+        PROCESS_BASIC_INFORMATION_FOR_PEB ProcBasicInfo;
+        UINT_PTR ppTargetUserProcessParameters;
+        UINT_PTR pTargetUserProcessParameters = 0;
+        UNICODE_STRING_FOR_CMDLINE ustrTargetCommandLine = { 0, 0, NULL };
+        UINT_PTR pwszTargetCommandLine;
+
+        /* Skip PID 0 [System Process] */
+        if (pe32.th32ProcessID == 0) {
+            _tprintf(_T("PID %lu: %s\n"), pe32.th32ProcessID, pe32.szExeFile);
+            if (_tcscmp(pe32.szExeFile, _T("[System Process]"))) {
+                _ftprintf(stderr, _T("PID %lu (%s): Unexpected PID 0 process name\n"),
+                          pe32.th32ProcessID, pe32.szExeFile);
+                /* Make this error fatal in order to detect such a situation */
+                return 1;
+            }
+            continue;
+        }
 
         /* 32-bit applications fail to inject code into 64-bit ones and vice-versa.
          * Such issue can be detected when attempting to enumerate modules.
          */
         hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe32.th32ProcessID);
         if (hModuleSnap == INVALID_HANDLE_VALUE) {
-            if (GetLastError() == ERROR_NOACCESS) {
+            if (GetLastError() == ERROR_NOACCESS || GetLastError() == ERROR_PARTIAL_COPY) {
                 /* This happens if I use 32 bits and target uses 64 bits */
-                _tprintf(_T("PID %lu (%s): Unable to enumerate modules\n"), pe32.th32ProcessID, pe32.szExeFile);
+                _ftprintf(stderr, _T("PID %lu (%s): Unable to enumerate modules\n"),
+                          pe32.th32ProcessID, pe32.szExeFile);
             } else {
+                _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
                 print_winerr(_T("CreateToolhelp32Snapshot(Module)"));
             }
             continue;
@@ -78,36 +153,124 @@ int _tmain(void)
         }
         CloseHandle(hModuleSnap);
 
-        /* Spawn a thread inside the process */
+        /* Open the target process */
         hProcess = OpenProcess(
-            PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+            PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
             0, pe32.th32ProcessID);
         if (!hProcess) {
             if (GetLastError() != ERROR_ACCESS_DENIED) {
-                _ftprintf(stderr, _T("%lu: access error:\n"), pe32.th32ProcessID);
+                _ftprintf(stderr, _T("PID %lu (%s): access error: "), pe32.th32ProcessID, pe32.szExeFile);
                 print_winerr(_T("OpenProcess"));
             }
             continue;
         }
+
+        /* Retrieve the PEB using ProcessBasicInformation = 0 */
+        ZeroMemory(&ProcBasicInfo, sizeof(ProcBasicInfo));
+        dwStatus = (*pfnNtQueryInformationProcess)(hProcess, 0, &ProcBasicInfo, sizeof(ProcBasicInfo), &length);
+        if (dwStatus) {
+            _ftprintf(stderr, _T("PID %lu (%s): NtQueryInformationProcess: error %#lx\n"),
+                      pe32.th32ProcessID, pe32.szExeFile, dwStatus);
+            CloseHandle(hProcess);
+            continue;
+        }
+        /* Get PEB->ProcessParameters (PRTL_USER_PROCESS_PARAMETERS) */
+        ppTargetUserProcessParameters = ((UINT_PTR)ProcBasicInfo.PebBaseAddress) + PEB_offset_ProcessParameters;
+        cbRead = 0;
+        if (!ReadProcessMemory(hProcess,
+                               (PVOID)ppTargetUserProcessParameters,
+                               &pTargetUserProcessParameters,
+                               sizeof(pTargetUserProcessParameters),
+                               &cbRead)) {
+            _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
+            print_winerr(_T("ReadProcessMemory(PEB->ProcessParameters)"));
+            CloseHandle(hProcess);
+            continue;
+        }
+        assert(cbRead == sizeof(pTargetUserProcessParameters));
+
+        /* Get ProcessParameters->CommandLine (UNICODE_STRING) */
+        cbRead = 0;
+        if (!ReadProcessMemory(hProcess,
+                               (PVOID)(pTargetUserProcessParameters + RTL_USER_PROCESS_PARAMETERS_offset_CommandLine),
+                               &ustrTargetCommandLine,
+                               sizeof(ustrTargetCommandLine),
+                               &cbRead)) {
+            _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
+            print_winerr(_T("ReadProcessMemory(ProcessParameters->CommandLine)"));
+            CloseHandle(hProcess);
+            continue;
+        }
+        assert(cbRead == sizeof(UNICODE_STRING_FOR_CMDLINE));
+
+        /* Read the command line */
+        if (ustrTargetCommandLine.Length >= sizeof(wszCmdLine)) {
+            _ftprintf(stderr, _T("PID %lu (%s): command line is too large (%u bytes)\n"),
+                      pe32.th32ProcessID, pe32.szExeFile, ustrTargetCommandLine.Length);
+            /* This is a fatal error: need to increase the size of the buffer */
+            return 1;
+        }
+        ZeroMemory(wszCmdLine, sizeof(wszCmdLine));
+        cbRead = 0;
+        if (!ReadProcessMemory(hProcess,
+                               ustrTargetCommandLine.Buffer,
+                               &wszCmdLine,
+                               ustrTargetCommandLine.Length,
+                               &cbRead)) {
+            _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
+            print_winerr(_T("ReadProcessMemory(CommandLine->Buffer)"));
+            CloseHandle(hProcess);
+            continue;
+        }
+        assert(cbRead == ustrTargetCommandLine.Length);
+        assert(cbRead < sizeof(wszCmdLine));
+        wszCmdLine[ustrTargetCommandLine.Length / 2] = 0;
+        _tprintf(_T("PID %lu[%s]: %" PRIsW "\n"), pe32.th32ProcessID, pe32.szExeFile, wszCmdLine);
+
+        /* Spawn a thread inside the process to get the command line through APIs */
         hThread = CreateRemoteThread(hProcess, NULL, 0, lpfctGetCmdLine, NULL, 0, NULL);
         if (!hThread) {
+            _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
             print_winerr(_T("CreateRemoteThread"));
             CloseHandle(hProcess);
             continue;
         }
-        WaitForSingleObject(hThread, INFINITE);
-        GetExitCodeThread(hThread, &dwRetAddr);
-        CloseHandle(hThread);
-
-        /* Assume the address of the command line fits a 32-bit pointer */
-        if (!ReadProcessMemory(hProcess, ULongToPtr(dwRetAddr), szCmdLine, sizeof(szCmdLine), &cbRead)) {
-            print_winerr(_T("ReadProcessMemory"));
-            CloseHandle(hProcess);
-            continue;
+        /* Detect suspended processes by waiting 1 second for the thread */
+        dwStatus = WaitForSingleObject(hThread, 1000);
+        if (dwStatus == WAIT_FAILED) {
+            _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
+            print_winerr(_T("WaitForSingleObject(remote thread)"));
+            return 1;
         }
-        assert(cbRead <= sizeof(szCmdLine));
-        szCmdLine[ARRAYSIZE(szCmdLine) - 1] = 0;
-        wprintf(L"PID %lu: %s\n", pe32.th32ProcessID, szCmdLine);
+        if (dwStatus == WAIT_TIMEOUT) {
+            if (!TerminateThread(hThread, 0)) {
+                _ftprintf(stderr, _T("PID %lu (%s): "), pe32.th32ProcessID, pe32.szExeFile);
+                print_winerr(_T("TerminateThread(remote suspended thread)"));
+                /* Fail now */
+                return 1;
+            }
+            CloseHandle(hThread);
+            _tprintf(_T("... process is suspended\n"));
+        } else if (dwStatus == WAIT_OBJECT_0) {
+            /* The thread terminated nicely and its exit code is the result of GetCommandLineW() */
+            GetExitCodeThread(hThread, &dwRetAddr);
+            CloseHandle(hThread);
+
+            pwszTargetCommandLine = (UINT_PTR)ustrTargetCommandLine.Buffer;
+            if (dwRetAddr != 0 && dwRetAddr != (pwszTargetCommandLine & 0xffffffff)) {
+
+                _ftprintf(stderr, _T("PID %lu (%s): Mismatching command line ptr: %#lx != LODWORD(%#" PRIxPTR ")\n"),
+                          pe32.th32ProcessID, pe32.szExeFile,
+                          dwRetAddr, pwszTargetCommandLine);
+                return 1;
+            }
+        } else {
+            _ftprintf(stderr, _T("PID %lu (%s): Unexpected WaitForSingleObject result: %#lx\n"),
+                      pe32.th32ProcessID, pe32.szExeFile, dwStatus);
+            return 1;
+        }
+
         CloseHandle(hProcess);
     } while (Process32Next(hProcessSnap, &pe32));
     CloseHandle(hProcessSnap);
