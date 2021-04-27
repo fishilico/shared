3d77b24faee7c0c9eb5dee9fa31bd66171568f84
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2021-04-27 09:26:36 +0200

    linux/nolibc: add new syscall from Linux 5.12
    fs: add mount_setattr()
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=2a1867219c7b27f928e2545782b86daaf9ad50bd

--- linux/nolibc/include/asm-x86_64/unistd.h
+++ linux/nolibc/include/asm-x86_64/unistd.h
@@ -354,9 +354,10 @@
 #define __NR_faccessat2 439
 #define __NR_process_madvise 440
 #define __NR_epoll_pwait2 441
+#define __NR_mount_setattr 442
 
 #ifdef __KERNEL__
-#define __NR_syscall_max 441
+#define __NR_syscall_max 442
 #endif
 
 #endif /* _ASM_X86_UNISTD_H */
