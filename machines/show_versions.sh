0c6b651b5766302eb1fe6529499868c7a8a648fe
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2022-03-28 21:46:50 +0200

    machines/show_versions: record the loader (glibc and musl) versions

--- machines/show_versions.sh
+++ machines/show_versions.sh
@@ -17,8 +17,18 @@ show_version x86_64-w64-mingw32-gcc "$(x86_64-w64-mingw32-gcc --version 2>/dev/n
 show_version i686-w64-mingw32-gcc "$(i686-w64-mingw32-gcc --version 2>/dev/null | head -n1)"
 show_version wine "$(wine --version 2>/dev/null)"
 
-# Linux kernel
+# Linux kernel and loaders
 show_version 'Linux kernel' "${KERNELVER:-$(uname -r)}"
+for LOADER in /lib/ld-linux.so.* ; do
+    if [ -e "$LOADER" ] ; then
+        show_version "$LOADER" "$("$LOADER" --version 2>/dev/null | head -n1)"
+    fi
+done
+for LOADER in /lib/ld-musl-*.so.* ; do
+    if [ -e "$LOADER" ] ; then
+        show_version "$LOADER" "$("$LOADER" --version 2>&1 | head -n2 | xargs)"
+    fi
+done
 
 # Python
 show_version python "$(python --version 2>/dev/null)"
