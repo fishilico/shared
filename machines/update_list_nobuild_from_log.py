6ce27be315d38644d31da0d3d3ab86af8196b3ed
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2022-05-29 12:15:10 +0200

    machines: update machines

--- machines/update_list_nobuild_from_log.py
+++ machines/update_list_nobuild_from_log.py
@@ -92,6 +92,7 @@ def parse_log_file(filepath):
                         '0017:err:service:process_send_command service protocol error - failed to read pipe r = 0  count = 0!',  # noqa
                         '007c:err:rpc:RpcAssoc_BindConnection receive failed with error 1726',
                         '008c:err:rpc:I_RpcReceive we got fault packet with status 0x1c010003',
+                        '009c:err:rpc:I_RpcReceive we got fault packet with status 0x1c010003',
                         '00a0:err:rpc:I_RpcReceive we got fault packet with status 0x1c010003',
                         ):
                     # Ignore lines generated from stray instances of Wine
