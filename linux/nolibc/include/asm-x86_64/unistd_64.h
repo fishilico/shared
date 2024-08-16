60dd28ed7f813cb264ed5ca72cae6046a8825ed3
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2024-08-16 09:16:28 +0200

    linux/nolibc/include: add new syscall from Linux 6.10
    "mseal: wire up mseal syscall"
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d8b0f5465012538cc4bb10ddc4affadbab73465b

--- linux/nolibc/include/asm-x86_64/unistd_64.h
+++ linux/nolibc/include/asm-x86_64/unistd_64.h
@@ -374,9 +374,10 @@
 #define __NR_lsm_get_self_attr 459
 #define __NR_lsm_set_self_attr 460
 #define __NR_lsm_list_modules 461
+#define __NR_mseal 462
 
 #ifdef __KERNEL__
-#define __NR_syscalls 462
+#define __NR_syscalls 463
 #endif
 
 #endif /* _UAPI_ASM_UNISTD_64_H */
