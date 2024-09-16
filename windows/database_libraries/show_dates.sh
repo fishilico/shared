ab3370514d6cc129dc2e3f0738fcc8cbb5cbd31b
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2024-09-16 20:25:27 +0200

    windows/database_libraries: fix jq expression to actually use Authenticode timestamp
    Using `select` made the filter ignore any file built with the
    `debug_reproducible` data. Replace it with a `if/else/end` construction
    to no longer ignore these files.
    
    Regenerate `file_dates.txt` with the new timestamps.

--- windows/database_libraries/show_dates.sh
+++ windows/database_libraries/show_dates.sh
@@ -6,8 +6,8 @@ cat "$(dirname -- "$0")"/*.db/*_versions.db.json | \
     jq 'to_entries |
         map(.key as $k | .value.versions | map(.name_arch = $k) | .[]) |
         .[] |
-        .peh_ts = (select(.debug_reproducible|not) | .pe_header.timestamp) |
-        .peh_ts_iso = (select(.debug_reproducible|not) | .pe_header.timestamp_iso) |
+        .peh_ts = (if .debug_reproducible then null else .pe_header.timestamp end) |
+        .peh_ts_iso = (if .debug_reproducible then null else .pe_header.timestamp_iso end) |
         .timestamp = (.authenticode[0].timestamp_counter_sign.timestamp // .peh_ts) |
         select(.timestamp) |
         .ts_iso = (.authenticode[0].timestamp_counter_sign.timestamp_iso // .peh_ts_iso)' |
