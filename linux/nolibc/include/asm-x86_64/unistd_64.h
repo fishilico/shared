a9a5781e10fa3c4bb30fbac7363b341de6cdc513
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2025-02-08 17:33:39 +0100

    linux/nolibc/include: add new syscall from Linux 6.13
    "fs/xattr: add *at family syscalls"
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=6140be90ec70c39fa844741ca3cc807dd0866394

--- linux/nolibc/include/asm-x86_64/unistd_64.h
+++ linux/nolibc/include/asm-x86_64/unistd_64.h
@@ -376,9 +376,13 @@
 #define __NR_lsm_set_self_attr 460
 #define __NR_lsm_list_modules 461
 #define __NR_mseal 462
+#define __NR_setxattrat 463
+#define __NR_getxattrat 464
+#define __NR_listxattrat 465
+#define __NR_removexattrat 466
 
 #ifdef __KERNEL__
-#define __NR_syscalls 463
+#define __NR_syscalls 467
 #endif
 
 #endif /* _UAPI_ASM_UNISTD_64_H */
