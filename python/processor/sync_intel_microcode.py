fishilico/shared

commit 3adcd03ac3997f4b0ea7b8d66d400988b7857bf4

python/processor/intel_microcode_versions: add Intel microcode versions
https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/releases/tag/microcode-20250812

    Security updates for INTEL-SA-01249 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01249.html
    Security updates for INTEL-SA-01308 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01308.html
    Security updates for INTEL-SA-01310 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01310.html
    Security updates for INTEL-SA-01311 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01311.html
    Security updates for INTEL-SA-01313 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01313.html
    Security updates for INTEL-SA-01367 https://www.intel.com/content/www/us/en/security-center/advisory/intel-sa-01367.html

python/processor/sync_intel_microcode.py
@@ -207,10 +207,16 @@ def parse_intel_ucode_releasenote_cfg(file_data: str) -> None:
             new_names = ["Intel® Core™ Processor with Hybrid Technology"]
         elif field_product_name in {
             "Core™ Ultra Processor",
-            "Core™ Ultra Processor (Series2)",
             "Core™ Ultra 200 V Series Processor",
         }:
             new_names = [f"Intel® {field_product_name}"]
+        elif field_product_name in {
+            "Core™ Ultra Processor (Series2)",
+            "Core Ultra Processor (Series 2)",
+        }:
+            new_names = [f"Intel® Core™ Ultra Processor (Series 2)"]
+        elif field_product_name == "Core Ultra 200 V Series Processor":
+            new_names = [f"Intel® Core™ Ultra 200 V Series Processor"]
         elif field_product_name == "Core i3-N305/N300, N50/N97/N100/N200, Atom x7211E/x7213E/x7425E":
             new_names = [
                 "Intel® Core i3-N305/N300",

commit c934fb789dad43b570ec98a9a574cd0a7760bfe5

python/processor/sync_intel_microcode: remove stray f-string prefix
Strings without format placeholders should not use f-string prefix.

Fixes: 3adcd03ac399 ("python/processor/intel_microcode_versions: add
Intel microcode versions")

python/processor/sync_intel_microcode.py
@@ -214,9 +214,9 @@ def parse_intel_ucode_releasenote_cfg(file_data: str) -> None:
             "Core™ Ultra Processor (Series2)",
             "Core Ultra Processor (Series 2)",
         }:
-            new_names = [f"Intel® Core™ Ultra Processor (Series 2)"]
+            new_names = ["Intel® Core™ Ultra Processor (Series 2)"]
         elif field_product_name == "Core Ultra 200 V Series Processor":
-            new_names = [f"Intel® Core™ Ultra 200 V Series Processor"]
+            new_names = ["Intel® Core™ Ultra 200 V Series Processor"]
         elif field_product_name == "Core i3-N305/N300, N50/N97/N100/N200, Atom x7211E/x7213E/x7425E":
             new_names = [
                 "Intel® Core i3-N305/N300",