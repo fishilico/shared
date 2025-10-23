fishilico/shared

commit 574a883c9fb80a34706e2ba0d8fb840cd5ca8e34

shellcode/reformat_shellcodes: support 'lods   (%rsi),%rax' without segment prefix 'ds:'
binutils 2.45 removed default segment prefixes:
https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=36fa5275c164c3eea585ff0a5aaefc235a18b298;hp=77ad112d8cc8056e9c99d1c064c773d96fb2dc12
(or on GitHub:
https://github.com/bminor/binutils-gdb/commit/36fa5275c164c3eea585ff0a5aaefc235a18b298)

Before:

    lods   %ds:(%rsi), %rax

After:

    lods   (%rsi),%rax

shellcode/reformat_shellcodes.py
@@ -169,9 +169,15 @@ def reformat_shc_with_file(filename, objdumpcmd):
 
             # objdump 2.42 use "shr $1,%edx" instead of "shr %edx",
             # otherwise there will be correctness issues when it is extended to support APX NDD
-            # https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=b70a487d5945b13e5ab503be4fc37b964819ec0e
+            # https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=b70a487d5945b13e5ab503be4fc37b964819ec0e
             if asmfile.endswith(("x86_32.S", "x86_64.S")) and disasm_instr == "shr    $1,%edx":
                 disasm_instr = "shr    %edx"
+
+            # objdump 2.45 removed default segment prefixes for string
+            # instructions such as lods, in 64-bit mode
+            # https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=36fa5275c164c3eea585ff0a5aaefc235a18b298
+            if asmfile.endswith("x86_64.S") and disasm_instr == "lods   (%rsi),%rax":
+                disasm_instr = "lods   %ds:(%rsi), %rax"
 
             if not instr_match(asminstr, disasm_instr):
                 sys.stderr.write("Instructions did not match: {} vs {}\n"

commit 858e8012f24cf1194c739118985157fb10cf7ffd

shellcode/reformat_shellcodes: support 'lods   (%rsi),%al' and 'rep stos %al,(%rdi)' without segment prefixes
Previous commit only added support of the 32-bit load variant of lods.
The Windows shellcode also uses the 8-bit one, as well as "rep stos"

Fixes: 574a883c9fb8 ("shellcode/reformat_shellcodes: support
'lods   (%rsi),%rax' without segment prefix 'ds:'")

shellcode/reformat_shellcodes.py
@@ -174,10 +174,15 @@ def reformat_shc_with_file(filename, objdumpcmd):
                 disasm_instr = "shr    %edx"
 
             # objdump 2.45 removed default segment prefixes for string
-            # instructions such as lods, in 64-bit mode
+            # instructions such as lods and stos, in 64-bit mode
             # https://sourceware.org/git/?p=binutils-gdb.git;a=commitdiff;h=36fa5275c164c3eea585ff0a5aaefc235a18b298
-            if asmfile.endswith("x86_64.S") and disasm_instr == "lods   (%rsi),%rax":
-                disasm_instr = "lods   %ds:(%rsi), %rax"
+            if asmfile.endswith("x86_64.S"):
+                if disasm_instr == "lods   (%rsi),%rax":
+                    disasm_instr = "lods   %ds:(%rsi), %rax"
+                elif disasm_instr == "lods   (%rsi),%al":
+                    disasm_instr = "lods   %ds:(%rsi), %al"
+                elif disasm_instr == "rep stos %al,(%rdi)":
+                    disasm_instr = "rep stos %al, %es:(%rdi)"
 
             if not instr_match(asminstr, disasm_instr):
                 sys.stderr.write("Instructions did not match: {} vs {}\n"